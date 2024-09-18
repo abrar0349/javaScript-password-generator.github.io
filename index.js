2b6dde539e2acdbc78065a74891ce1fe24030a50
abrar0349 <brar0349khan@gmail.com>
2024-09-18 15:12:18 +0500

    "More work on password generator"

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 let inputSlider = document.getElementById('inputSlider')
 let sliderValue = document.getElementById('sliderValue')
-let passBox = document.getElementsByClassName('passBox')
+let passBox = document.getElementsByClassName('passBox')[0]
 let lowercase = document.getElementById('lowercase')
 let uppercase = document.getElementById('uppercase')
 let number = document.getElementById('number')
 let symbols = document.getElementById('symbols')
+let genBtn = document.getElementById('genBtn')
 
-console.log(inputSlider,sliderValue,passBox,lowercase,uppercase,number,symbols)
+
+// console.log(inputSlider,sliderValue,passBox,lowercase,uppercase,number,symbols,genBtn)
 
 sliderValue.textContent = inputSlider.value;
 
 inputSlider.addEventListener('input', (e) => {
     sliderValue.textContent = inputSlider.value;
 })
+
+genBtn.addEventListener('click', () => {
+    console.log('hello')
+    passBox.value = generatPass()
+})
+
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let Numbers = "0123456789";
+let Saymbols = "!#$%&()*+,-./:;<=>?@[\]^_{|}~"
+
+
+function generatPass(){
+    let genPassword = "";
+    let allChars = "";
+    // following line is about random alphabit charactor
+ 
+    
+    allChars += lowercase.checked ? lowerChars : "";
+    allChars += uppercase.checked ? upperChars : "";
+    allChars += number.checked ? Numbers : "";
+    allChars += symbols.checked ? Saymbols : "";
+
+    if(allChars == "" || allChars.length == 0){
+        return genPassword
+    }
+
+    let i = 0;
+    while(i <= inputSlider.value){
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length))
+        i++
+    }
+
+
+    return genPassword
+}
